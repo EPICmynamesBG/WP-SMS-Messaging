8124d446d03f17fe7129807ed173a2a32fd9e7ef
EPICmynamesBG <mynamesbg@gmail.com>
2016-01-31 18:55:19 -0500

    updated to use built in mail for sending

--- sendMessage.php
+++ sendMessage.php
@@ -2,24 +2,25 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
+
 include_once('../../../wp-load.php');
-include_once('./SMSLookup.php');
 
 global $wpdb;
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST'){
-    sendMessage();
+    $settings = getSettings();
+    sendMessage($settings);
 } else {
     echo json_encode(array("status"=>"error",
                               "message"=>"Bad HTTP method"));
     die("Incorrect HTTP Method");
 }
 
-function getNumbers(){
+function getMailList(){
     global $wpdb;
     $table_name = $wpdb->prefix . "SMS";
 
-    $sql = "SELECT `sms_email` FROM `$table_name` WHERE 1;";
+    $sql = "SELECT * FROM `$table_name` WHERE 1;";
     $result = $wpdb->get_results($sql);
     if(sizeof($result) == 0 || $result == null){
         echo json_encode(array("status"=>"error",
@@ -28,26 +29,47 @@ function getNumbers(){
     }
     return $result;
 }
-//
-//function getAccountInfo(){
-//    global $wpdb;
-//    $table_name = $wpdb->prefix . "SMS_config";
-//
-//    $sql = "SELECT * FROM `$table_name` WHERE 1;";
-//    $result = $wpdb->get_results($sql);
-//    if(sizeof($result) == 0 || $result == null){
-//        echo json_encode(array("status"=>"error",
-//                              "message"=>"No account info saved."));
-//        die();
-//    }
-//    return $result[0];
-//}
 
-function sendMessage(){
-    $numberArr = getNumbers();
+function getSettings(){
+    global $wpdb;
+    $table_name = $wpdb->prefix . "SMS_config";
+
+    $sql = "SELECT * FROM `$table_name` WHERE 1;";
+    $result = $wpdb->get_results($sql);
+    if(sizeof($result) == 0 || $result == null){
+        echo json_encode(array("status"=>"error",
+                              "message"=>"No account info saved."));
+        die();
+    }
+    return $result[0];
+}
+
+function sendMessage($settings){
+    $numberArr = getMailList();
     $resultsArr = array();
-    foreach($numberArr as $row){
+    $subject = "Practice Reminder";
+    $message = $_POST['message'];
+    $from = $settings->from_email;
 
+    $headers = "From: ".$from . "\r\n".
+        'X-Mailer: PHP/' . phpversion();
+
+    foreach($numberArr as $row){
+        $to = $row->sms_email;
+        $results = mail("bgroff@bsu.edu", $subject, $message, $headers, "-f$from");
+        $temp = array(
+            'mailStatus'=> $results,
+            'number'=> $row->phone_number,
+            'sms_email'=> $row->sms_email,
+            'mailContents'=> array(
+                'to' => $to,
+                'subject' => $subject,
+                'message' => $message,
+                'headers' => $headers
+            )
+        );
+
+        array_push($resultsArr, $temp);
     }
     echo json_encode($resultsArr);
 }
